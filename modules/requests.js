af7a288dd8db66d00de6c80d36c803890608e460
John Mcdowall <slushnix@gmail.com>
2023-03-24 10:59:56 -0400

    E

--- modules/requests.js
+++ modules/requests.js
@@ -4,19 +4,19 @@ const moment = require('moment');
 const axios = require('axios');
 
 async function getCosmeticJSON(cosmetic) {
-    const response = await axios.get(`https://fortnite-api.com/v2/cosmetics/br/${cosmetic}`);
-    const data = response.data;
-    var JsonString = JSON.stringify(data, null, 2);
+  const response = await axios.get(`https://fortnite-api.com/v2/cosmetics/br/${cosmetic}`);
+  const data = response.data;
+  var JsonString = JSON.stringify(data, null, 2);
 
-    return JSON.parse(JsonString, null, 2);
+  return JSON.parse(JsonString, null, 2);
 }
 
 async function getSetJSON(set) {
-    const response = await axios.get(`https://fortnite-api.com/v2/cosmetics/br/?set=${set}`);
-    const data = response.data;
-    var JsonString = JSON.stringify(data, null, 2);
+  const response = await axios.get(`https://fortnite-api.com/v2/cosmetics/br/search/all?set=${set}`);
+  const data = response.data;
+  var JsonString = JSON.stringify(data, null, 2);
 
-    return JSON.parse(JsonString, null, 2);
+  return JSON.parse(JsonString, null, 2);
 }
 
 module.exports = {
